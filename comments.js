ca036050b96fa8806930ec19d244beb3eb2309b1
CaffeinatedCass <65289600+cassandraetter@users.noreply.github.com>
2024-04-11 13:19:54 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,44 @@
+// Create web server
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', async (req, res) => {
+  const comment = req.body;
+
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push(comment);
+  commentsByPostId[req.params.id] = comments;
+
+  await axios.post('http://event-bus-srv:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      ...comment,
+      postId: req.params.id,
+    },
+  });
+
+  res.status(201).send(comments);
+});
+
+app.post('/events', (req, res) => {
+  console.log('Received Event:', req.body.type);
+
+  res.send({});
+});
+
+app.listen(4001, () => {
+  console.log('Listening on 4001');
+});
